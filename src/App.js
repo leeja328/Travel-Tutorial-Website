7936723130cde14413a2c2620a09574e1dd252b6
James <leeja328@gmail.com>
2022-11-01 19:22:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path='/services' element={<Services />}/>
+            <Route exact path='/products' element={<Products />}/>
+            <Route exact path='/sign-up' element={<SignUp />}/>
+          </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
